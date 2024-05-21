13b85046970697b8327c4f42980597d0b1e1eaa8
isratest <israelferic@gmail.com>
2024-05-20 20:46:54 -0400

    first commit - basic - Tailwind installed, Vite installed, and ready to use.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="bg-teal-400">
+        <h1>Hola Mundo bebé!</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
